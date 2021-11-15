a5a8c2c2039774362b9be0725b07bb64439c2ed9
rvxt21 <78904227+rvxt21@users.noreply.github.com>
2021-11-15 20:33:23 +0200

    1

---
+++ 16lab/16lab/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _16lab
+{
+    class Program
+    {
+        static int tableWidth = 73;
+        static void Main(string[] args)
+        {
+            Student st = new Student("Ivan", "Safontev", 18, 231);
+            Student st1 = new Student("Egor", "Dolgushov", 18, 231);
+            Student st2 = new Student("Anna", "Ivanova", 18, 211);
+            Console.Clear();
+            PrintLine();
+            AlignCentre("List of students", 5);
+            PrintLine();
+            PrintRow("Name", "Surname", "Age", "Group");
+            PrintLine();
+            PrintRow(st.Name, st.Surname,st.Age.ToString(),st.Group.ToString());
+            PrintLine();
+            PrintRow(st1.Name,st1.Surname,st1.Age.ToString(),st1.Group.ToString());
+            PrintLine();
+            PrintRow(st2.Name, st2.Surname, st2.Age.ToString(), st2.Group.ToString());
+            PrintLine();
+            Console.ReadLine();
+        }
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+        static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+    }
+}
+
---
+++ 16lab/16lab/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16lab
+{
+    class Student
+    {
+        private string name;
+        private string surname;
+        private int age;
+        private int group;
+        public Student(string name,string surname,int age,int group)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.group = group;
+        }
+        public Student()
+        {
+            Console.WriteLine("Enter name student:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter surname student:");
+            surname = Console.ReadLine();
+            Console.WriteLine("Enter age of student:");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter group of student:");
+            group = Convert.ToInt32(Console.ReadLine());
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+    }
+}
